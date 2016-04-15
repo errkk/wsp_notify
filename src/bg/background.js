ed1ef0cb2cb7f5838c67bc81f5d16ccbb131624f
Eric George <eric@pokelondon.com>
2016-04-15 12:41:50 +0100

    new api

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,41 +1,18 @@
-var pusher = new Pusher('db1b62b7ae963a6c8e55');
-var channel = pusher.subscribe('pump');
+var pusher = new Pusher('db1b62b7ae963a6c8e55', { encrypted: true });
+var channel = pusher.subscribe('panel');
 
-channel.bind('on', function(data) {
-    var havePermission = window.webkitNotifications.checkPermission();
-    if (havePermission == 0) {
-        // 0 is PERMISSION_ALLOWED
-        var notification = window.webkitNotifications.createNotification(
-            'img/on.png',
-            'Pump On',
-            'Uplift: ' + data + 'ºC'
-            );
+channel.bind('pump', function(data) {
+    var icon = {'On': 'on.png', 'Off': 'off.png'}[data];
+    var notification = chrome.notifications.create('Notification title', {
+            iconUrl: 'img/' + icon,
+            title: 'Pump',
+            message: 'Pump ' + data,
+            type: 'basic'
+        });
 
-        notification.onclick = function () {
-            window.open("http://wottonpool.co.uk/panel/data/");
-            notification.close();
-        }
-        notification.show();
-    } else {
-        window.webkitNotifications.requestPermission();
+    notification.onclick = function () {
+        window.open("http://wottonpool.co.uk/panel/data/");
+        notification.close();
     }
-});
-channel.bind('off', function(data) {
-    var havePermission = window.webkitNotifications.checkPermission();
-    if (havePermission == 0) {
-        // 0 is PERMISSION_ALLOWED
-        var notification = window.webkitNotifications.createNotification(
-            'img/off.png',
-            'Pump Off',
-            'Uplift: ' + data + 'ºC'
-            );
-
-        notification.onclick = function () {
-            window.open("http://wottonpool.co.uk/panel/data/");
-            notification.close();
-        }
-        notification.show();
-    } else {
-        window.webkitNotifications.requestPermission();
-    }
+    notification.show();
 });
